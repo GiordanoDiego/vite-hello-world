bf8be6c3d479cfbd537129d45e0f7cd1b795620f
GiordanoDiego <diegomgiordano96@gmail.com>
2024-01-17 17:19:09 +0100

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,41 @@
 <script>
+/*
+    utilizzare un componente dentro un altro
+    1) importazione del componente
+    2) dichiarazione del componente all'interno dell'oggetto
+    3) utilizzo del componente
+*/
 
+// 1) importazione
+import AppHeader from './components/AppHeader.vue';
+    export default {
+        data() {
+            return { 
+                count: 0 
+            }
+        },
+        // 2) Dichiaro il componente
+        components:{
+            AppHeader
+        }
+    }
 </script>
 
 <template>
- 
+    <div>
+        <!--3 step dichiarano il componente avremo un tag che corrisponde al nostro componente
+            e quindi sparera al posto del componente il codice contenten nel componente
+        -->
+        <AppHeader/> 
+        <!-- qui scriverà il codice del componente -->
+        <p>
+            {{ count }}
+        </p>
+    </div>
+    <button @click="count++">
+        Incrementa
+    </button>
+  
 </template>
 
 <style scoped>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script>
+    export default {
+        data() {
+            return { 
+                title: 'ciao ragazzi'
+            }
+        },
+    }
+</script>
+
+<template>
+    <header>
+        <h1>
+            {{ title }}
+        </h1>
+    </header>
+  
+</template>
+
+<style scoped> /* qui ci vanno le classi che sono visibili solo a questo componente*/
+    h1{
+        color:brown;
+        text-align: center;
+        background-color: aquamarine;
+    }
+</style>
